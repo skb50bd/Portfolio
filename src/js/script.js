df98840e28f1d65627ab1d5fbbad7dd9d7a3d87f
Shakib Haris <skb50bd@users.noreply.github.com>
2020-07-29 02:40:19 +0600

    Skillbars animated with js

--- src/js/script.js
+++ src/js/script.js
@@ -1,71 +1,77 @@
-function debounce(func, wait, immediate) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
+const animateBar = (element, to) => {
+    let i = 0;
+    let interval = 10;
+    function move() {
+        if (i == 0) {
+            i = 1;
+            let width = 1;
+            let id = setInterval(frame, interval);
+            function frame() {
+                if (width >= to) {
+                    clearInterval(id);
+                    i = 0;
+                } else {
+                    width++;
+                    element.style.width = width + "%";
+                }
+            }
+        }
+    }
+    move();
+};
+
+const resetSkillsProgress = () => {
+    const progresses = document.querySelectorAll(".progress-base>div");
+    progresses.forEach(progress => progress.style.width = 0 + "%");
+};
+
+let animatingSkills = false;
+const populateSkills = () => {
+    if (animatingSkills) return;
+    animatingSkills = true;
+
+    const skills = document.querySelectorAll("#skillset .skill");
+
+    skills.forEach((skill, index) => {
+        const name = skill.dataset.skillName;
+        const percent = skill.dataset.skillPercent;
+
+        skill.getElementsByClassName("skill-name")[0].innerHTML = name;
+
+        setTimeout(
+            () => animateBar(skill.querySelector(".progress-base>div"), percent), 
+            index * percent * 5);
+
+        setTimeout(
+            () => skill.getElementsByClassName("skill-percent")[0].innerHTML = percent + "%", 
+            (index + 1) * percent * 5);
+    });
+
+    setTimeout(() => animatingSkills = false, skills.length * 500);
 };
 
 document.addEventListener(
     'DOMContentLoaded',
     () => {
+        populateSkills();
         // loadParticleJs("particles");
-
-        // Reads out the scroll position and stores it in the data attribute
-        // so we can use it in our stylesheets
-        const storeScroll = () => {
-            document.documentElement.dataset.scroll = window.scrollY;
-        }
-
-        // Listen for new scroll events, here we debounce our `storeScroll` function
-        document.addEventListener('scroll', debounce(storeScroll), { passive: true });
-
-        // Update scroll position for first time
-        storeScroll();
-
-        const skills = {
-            html: 95,
-            css: 90,
-            javascript: 70,
-            php: 55,
-            angular: 65
-        };
-
-        let multiply = 4;
-
-        Object.keys(skills).forEach(key => {
-            const delay = 500;
-
-            setTimeout(() => {
-                document.getElementById(key + '-percent').innerHTML = skills[key] + "%";
-            }, delay * multiply);
-
-            multiply++;
-        });
 
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
                 const id = entry.target.getAttribute('id');
                 if (entry.intersectionRatio > 0) {
                     document.querySelector(`nav li a[href="#${id}"]`).classList.add('active');
+                    if (id === "skills") {
+                        resetSkillsProgress();
+                        populateSkills();
+                    }
                 } else {
                     document.querySelector(`nav li a[href="#${id}"]`).classList.remove('active');
                 }
             });
         });
-    
+
         document.querySelectorAll('section[id]').forEach(section => {
             observer.observe(section);
         });
- 
-        executeAfter(
-            () => console.log("This is my awesome portfolio"),
-            2);
     });
