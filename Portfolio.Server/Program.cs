c400d098642ca8270af9eb80747c4b98252e0f4f
Shakib Haris <skb50bd@gmail.com>
2022-12-02 15:18:59 +0600

    Cleanup Program.cs

--- Portfolio.Server/Program.cs
+++ Portfolio.Server/Program.cs
@@ -1,11 +1,5 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-// var options = new DefaultFilesOptions();
-// options.DefaultFileNames.Clear();
-// options.DefaultFileNames.Add("index.html");
+var app = WebApplication.CreateBuilder(args).Build();
+app.MapGet("/ping", () => "ping");
 app.UseDefaultFiles();
 app.UseStaticFiles();
-app.MapGet("/ping", () => "ping");
-
 app.Run();
