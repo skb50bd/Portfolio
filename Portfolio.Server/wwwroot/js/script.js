fc049c089fdb6da45c6468f7e2435ec337207b3c
Shakib Haris <skb50bd@gmail.com>
2023-02-25 17:21:14 +0600

    Added Contact Section

--- Portfolio.Server/wwwroot/js/script.js
+++ Portfolio.Server/wwwroot/js/script.js
@@ -1,91 +1,47 @@
-import { mountSkillParticles, mountIntroParticles, adjustCanvasHeights } from "./particle-anims.js";
+var observer = new IntersectionObserver(entries => {
+    let maxSection = null;
+    let maxRatio = 0;
 
-const animateBar = (element, maxWidth, callback) => {
-    const interval = 10;
-
-    const move = () => {
-        let width = 1;
-        const frame = () => {
-            if (width >= maxWidth) {
-                clearInterval(id);
-                callback();
-            } else {
-                width++;
-                element.style.width = width + "%";
-            }
-        }
-        const id = setInterval(frame, interval);
-    }
-
-    move();
-};
-
-const writeSkillLabels = () => {
-    const skills = document.querySelectorAll("#skillset .skill");
-    skills.forEach(skill => skill.querySelector(".skill-name").innerHTML = skill.dataset.skillName);
-}
-
-const resetSkillsProgress = () => {
-    if (animatingSkills) return;
-    const progresses = document.querySelectorAll(".progress-base > div");
-    progresses.forEach(progress => progress.style.width = 0 + "%");
-
-    const progressValues = document.querySelectorAll(".skill-percent");
-    progressValues.forEach(elem => elem.innerHTML = "");
-};
-
-let animatingSkills = false;
-const writeSkillLevels = () => {
-    if (animatingSkills) return;
-    animatingSkills = true;
-
-    const skills = document.querySelectorAll("#skillset .skill");
-    let delay = 0;
-
-    skills.forEach(skill => {
-        const percent = skill.dataset.skillPercent;
-
-        delay += percent * 5;
-        setTimeout(
-            () => animateBar(
-                skill.querySelector(".progress-base > div"),
-                percent,
-                () => skill.querySelector(".skill-percent").innerHTML = percent + "%"),
-            delay);
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > maxRatio) {
+        maxSection = entry.target.getAttribute('id');
+        maxRatio = entry.intersectionRatio;
+      }
     });
 
-    setTimeout(() => animatingSkills = false, delay);
-};
+    const navLinks = document.querySelectorAll('a.nav-link');
+    navLinks.forEach(navLink => {
+      const id = navLink.getAttribute('href').substr(1);
+      if (id === maxSection) {
+        navLink.classList.add('active');
+      } else {
+        navLink.classList.remove('active');
+      }
+    });
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        if (entry.intersectionRatio > 0) {
-            document.querySelector(`nav li a[href="#${id}"]`).classList.add('active');
-            
-            if (id === "skills" && !animatingSkills) {
-                resetSkillsProgress();
-                writeSkillLevels();
-                adjustCanvasHeights();
-            }
-        } else {
-            document.querySelector(`nav li a[href="#${id}"]`).classList.remove('active');
-        }
+    console.log('Max section:', maxSection);
+  }, {
+    threshold: 0.5
+  }
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('section[id]').forEach(section => {
+        observer.observe(section);
     });
 });
 
-document.addEventListener(
-    'DOMContentLoaded',
-    () => {
-        mountIntroParticles();
-        mountSkillParticles();
+window.addEventListener('scroll', () => {
+    let sections = document.querySelectorAll('section');
+    let nav = document.querySelector('div.nav');
+    let currentSectionColor = '';
 
-        writeSkillLabels();
-        writeSkillLevels();
-
-        document.querySelectorAll('section[id]').forEach(section => {
-            observer.observe(section);
-        });
-
-        window.onresize = adjustCanvasHeights;
+    sections.forEach((section, index) => {
+      var sectionTop = section.offsetTop - 72;
+      if (window.scrollY >= sectionTop) {
+        currentSectionColor = section.style.background;
+      }
     });
+
+    nav.style.backgroundColor = currentSectionColor;
+  });
